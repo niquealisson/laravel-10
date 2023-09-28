080c147603ec183984b84a20765658da476a8a7a
niquealisson <alisson_201083@hotmail.com>
2023-09-28 18:30:18 -0300

    main

--- resources/views/admin/supports/partials/header.blade.php
+++ resources/views/admin/supports/partials/header.blade.php
@@ -2,8 +2,6 @@
     <div>
         <div class="flex items-center gap-x-3">
             <h1 class="text-lg text-black-500">Fórum</h1>
-            <a class="text-lg text-black-500" href="/profile"> Perfil</a>
-
             <span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">{{ $supports->total() }} dúvidas</span>
         </div>
     </div>
