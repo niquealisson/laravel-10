237094090c6681fdfa4e178cafee3571da50e3d6
niquealisson <alisson_201083@hotmail.com>
2023-09-30 13:06:43 -0300

    main

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
+            <a href="/supports"><strong> Voltar para o Fórum </strong></a>
         </h2>
     </x-slot>
 
